2ed5aec0c3199ac116ed64767ccdb527cff493ad
Sonali Rawat <programmergithub789@gmail.com>
2023-11-14 20:40:48 +0530

    updating store's index file

--- netflix-ui/src/store/index.js
+++ netflix-ui/src/store/index.js
@@ -1,18 +1,33 @@
 import{
-    configureStore, createAsyncThunk,
+    configureStore, 
+    createAsyncThunk,
     createSlice,
 } from "@reduxjs/toolkit";
+import axios from "axios";
+import {API_KEY,TMDB_BASE_URL} from "../utils/constants";
 
 const initialState={
     movies:[],
     genresLoaded:false,
     genres:[],
 };
+export const getGenres=createAsyncThunk("netflix/genres", async ()=>{
+const {data} = await axios.get(
+    `${TMDB_BASE_URL}/genre/movie/list ?api_key${API_KEY}`
+);
+ console.log(data);
+//  return data
+});
 
 const NetflixSlice=createSlice({
     name:"Netflix",
     initialState,
-    extraReducers:(builder)=>{},
+    extraReducers:(builder)=>{
+        builder.addCase(getGenres.fulfilled,(state,action)=>{
+            state.genres=action.payload;
+            state.genresLoaded=true;
+        });
+    },
 });
 export const store = configureStore({
     reducer:{
